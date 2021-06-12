6246f5139b5c60c94b4d71888fa576bec90aab36
alexsandrrh <sascha.sadov@yandex.ru>
2021-06-12 20:21:22 +0300

    update config rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 import resolve from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
@@ -16,6 +18,8 @@ export default {
   ],
   plugins: [
     resolve(),
+    commonjs(),
+    babel({ babelHelpers: "bundled" }),
     typescript({
       typescript: require("typescript"),
     }),
