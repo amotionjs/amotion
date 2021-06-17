fc2adf3e5e002df9dfba73c38a7c43d353f963f1
alexsandrrh <sascha.sadov@yandex.ru>
2021-06-17 20:38:25 +0300

    create methods run & destroy

--- src/Amotion.ts
+++ src/Amotion.ts
@@ -1,3 +1,23 @@
-class Amotion {}
+import { createServer, Server } from 'http';
+
+class Amotion {
+  // Создаем сервер
+  private _server: Server = createServer();
+
+  // Запуск приложения
+  public run = (port: number): Promise<void> =>
+    new Promise((resolve, reject) => {
+      this._server.listen(port, resolve).on('error', reject);
+    });
+
+  // Остановка приложения
+  public destroy = (): Promise<void> =>
+    new Promise((resolve, reject) => {
+      this._server.close((err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+}
 
 export default Amotion;
